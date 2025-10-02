5037628bbe8f19c805abe07ab34f89a88c1835eb
Charl Kivioja <charl.kivioja@gmail.com>
2025-10-02 17:22:28 +0300

    add lwjgl and render hello world window

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,34 @@ repositories {
     mavenCentral()
 }
 
+val lwjglVersion = "3.3.6"
+val lwjglNatives = "natives-linux"
+
 dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-bgfx")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-nuklear")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-par")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-vulkan")
+    implementation("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-bgfx", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
