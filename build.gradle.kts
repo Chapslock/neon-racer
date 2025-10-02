7038764be997cdf80825630e66bde4b86961b8be
Charl Kivioja <charl.kivioja@gmail.com>
2025-10-02 22:38:43 +0300

    create jpackage gradle task for creating native desktop applications and installers for all OS-s

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,32 @@
+import org.gradle.internal.jvm.Jvm
+import java.util.Locale
+
 plugins {
     id("java")
     id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 application {
     mainClass = "org.chapzlock.Main"
+    applicationDefaultJvmArgs = listOf("--enable-native-access=ALL-UNNAMED")
 }
 
 group = "org.chapzlock"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 val lwjglVersion = "3.3.6"
-val lwjglNatives = "natives-linux"
+val osName = System.getProperty("os.name").lowercase(Locale.getDefault())
+val lwjglNatives =
+    when {
+        osName.contains("win") -> "natives-windows"
+        osName.contains("mac") -> "natives-macos"
+        else -> "natives-linux"
+    }
 
 dependencies {
     implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
@@ -49,4 +60,78 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register<Exec>("jpackageApp") {
+    dependsOn(tasks.shadowJar)
+
+    val jpackageExecutable =
+        Jvm
+            .current()
+            .javaHome
+            .resolve("bin")
+            .resolve("jpackage")
+            .absolutePath
+    val appName = "NeonRacer"
+    val mainClass = "org.chapzlock.Main"
+    val jarName = "neon-racer-$version-all.jar"
+    val inputDir =
+        layout.buildDirectory
+            .dir("libs")
+            .get()
+            .asFile.absolutePath
+    val outputDir =
+        layout.buildDirectory
+            .dir("jpackage")
+            .get()
+            .asFile.absolutePath
+    val appDir = file("$outputDir/$appName")
+
+    doFirst {
+        if (appDir.exists()) {
+            appDir.deleteRecursively()
+        }
+        mkdir(outputDir)
+    }
+
+    var installerType = "app-image"
+    /*
+    val os =
+        org.gradle.internal.os.OperatingSystem
+            .current()
+    if (os.isWindows) {
+        installerType = "exe"
+    }
+    if (os.isMacOsX) {
+        installerType = "dmg"
+    }
+    if (os.isLinux) {
+        installerType = "deb"
+    }
+     */
+
+    commandLine(
+        jpackageExecutable,
+        "--name",
+        appName,
+        "--input",
+        inputDir,
+        "--main-jar",
+        jarName,
+        "--main-class",
+        mainClass,
+        "--type",
+        installerType,
+        "--dest",
+        outputDir,
+    )
+    val jvmArgs = application.applicationDefaultJvmArgs
+    jvmArgs.forEach { arg ->
+        commandLine(commandLine + listOf("--java-options", arg))
+    }
+    /*
+    if (os.isWindows) {
+        commandLine(commandLine + listOf("--win-shortcut", "--win-menu"))
+    }
+     */
 }
